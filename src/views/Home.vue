77268861d5a720730319fbdf117d2a5b23fbd3a9
oldflood <hhq@moreyou.cn>
2024-06-18 17:26:07 +0800

    init project

---
+++ src/views/Home.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import Cursor from "../components/kits/CustomCursor.vue";
+</script>
+
+<template>
+  <cursor/>
+</template>
+<style lang="scss" scoped>
+
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import {fileURLToPath, URL} from 'node:url'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+        host: '0.0.0.0'
+    },
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: '@import"@/assets/scss/index.scss";'
+            }
+        }
+    },
+})
